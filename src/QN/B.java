80535a2077beb36ab7fe71192db2022687b5b459
T_T <92500232+u2000670@users.noreply.github.com>
2022-03-16 20:17:21 +0800

    Update B.java

--- src/QN/B.java
+++ src/QN/B.java
@@ -5,61 +5,6 @@ import java.util.Scanner;
 
 public class B {
 
-    static int randomBinaryDigit() {
-
-        int num = ((int) (Math.random() * 100) + 1) % 2;
-        return num;
-    }
-
-    static String randomBinaryNum(int N) {
-
-        String str = "";
-        for (int i = 0; i < N; i++) {
-
-            int x = randomBinaryDigit();
-            str += String.valueOf(x);
-        }
-        return str;
-    }
-
-    static void randomIps() {
-        Random r = new Random();
-
-        int[] ipDec = new int[4];
-        for (int i = 0; i < ipDec.length; i++) {
-            ipDec[i] = r.nextInt(256);
-            if (i == 3) {
-                System.out.println(ipDec[i]);
-                break;
-            }
-            System.out.print(ipDec[i] + ".");
-        }
-
-        String[] ipBin = new String[4];
-        for (int i = 0; i < ipBin.length; i++) {
-            ipBin[i] = randomBinaryNum(8);
-            if (i == 3) {
-                System.out.println(ipBin[i]);
-                break;
-            }
-            System.out.print(ipBin[i] + ".");
-        }
-
-        int same = 0;
-        for (int i = 0; i < ipBin.length; i++) {
-            String convertedBin = Integer.toBinaryString(ipDec[i]);
-            if (convertedBin.equals(ipBin[i])) {
-                same++;
-            }
-        }
-
-        if (same == 4) {
-            System.out.println(">>>>>> Yes");
-        } else {
-            System.out.println("****** No");
-        }
-    }
-
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         
